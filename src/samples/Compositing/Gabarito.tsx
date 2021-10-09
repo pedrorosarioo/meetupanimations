19b36c2061d77d95e5ab39a504905293bc9506b9
pedrorosarioo <pedro.rosario@thouhtworks.com>
2021-10-08 22:58:52 -0300

    make compositing anymation easier to use

--- src/samples/Compositing/Gabarito.tsx
+++ src/samples/Compositing/Gabarito.tsx
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
   StyleSheet,
   View,
   TouchableOpacity,
   Text,
   Animated,
-  Easing
-} from 'react-native';
+  Easing,
+} from "react-native";
 
 /* 
 - callback start()
@@ -21,54 +21,64 @@ const translateY = new Animated.Value(0);
 
 const moveUp = Animated.timing(translateY, {
   toValue: 0,
-  duration: 500
+  duration: 500,
 });
 
 const moveDown = Animated.timing(translateY, {
   toValue: 290,
-  duration: 500
+  duration: 500,
 });
 
 const moveLeft = Animated.timing(translateX, {
   toValue: 0,
-  duration: 500
+  duration: 500,
 });
 
 const moveRight = Animated.timing(translateX, {
   toValue: 310,
   duration: 500,
-})
+});
 
 const handlePress = () => {
+  //just uncomment the function that you want to use
+
   // L
-  // const LMove = Animated.sequence([moveDown, moveRight, moveUp, moveLeft]);
+  const LMove = Animated.sequence([moveDown, moveRight, moveUp, moveLeft]);
   // LMove.start();
 
   // square
-  // const squareMove = Animated.sequence([moveDown, moveRight, moveUp, moveLeft]);
+  const squareMove = Animated.sequence([moveDown, moveRight, moveUp, moveLeft]);
   // squareMove.start();
 
   // square with delay
-  // const delayedSquareMove = Animated.stagger(600, [moveDown, moveRight, moveUp, moveLeft]);
+  const delayedSquareMove = Animated.stagger(600, [
+    moveDown,
+    moveRight,
+    moveUp,
+    moveLeft,
+  ]);
   // delayedSquareMove.start();
 
   // INFINITY LOOP
-  // const squareMove = Animated.sequence([moveDown, moveRight, moveUp, moveLeft]);
-  // const infinitySquare = Animated.loop(squareMove, {
-  //   iterations: -1
-  // });
+  const infinityLoopMove = Animated.sequence([
+    moveDown,
+    moveRight,
+    moveUp,
+    moveLeft,
+  ]);
+  const infinitySquare = Animated.loop(squareMove, {
+    iterations: -1,
+  });
   // infinitySquare.start();
-}
-
+};
 
 const Compositing = (props: IProps) => {
-
   return (
     <View style={styles.container}>
-      <Animated.View 
+      <Animated.View
         style={{
           ...styles.square,
-          transform: [{ translateX },{ translateY }]
+          transform: [{ translateX }, { translateY }],
         }}
       />
       <TouchableOpacity style={styles.button} onPress={handlePress}>
@@ -76,27 +86,27 @@ const Compositing = (props: IProps) => {
       </TouchableOpacity>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
+    alignItems: "center",
+    justifyContent: "center",
   },
   button: {
-    position: 'absolute',
+    position: "absolute",
     bottom: 40,
-    right: 90
+    right: 90,
   },
   square: {
     height: 40,
     width: 40,
-    backgroundColor: '#000',
-    position: 'absolute',
+    backgroundColor: "#000",
+    position: "absolute",
     top: 10,
-    left: 10
-  }
+    left: 10,
+  },
 });
 
 export default Compositing;
